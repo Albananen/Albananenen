ba60e46d1b7b7c8682c6090820ca4c7655156069
Albananen <04Niab@skola.engelholm.se>
2022-08-22 19:01:32 +0200

    a commit

---
+++ www/public/NAN.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head lang="sv"></head>
+<title>Home</title>
+<body>
+<form>
+    <label>Namn</label><br>
+    <input type="text" name="namn">
+    <br />
+    <label>Telefon</label><br />
+    <input type="text" name="telefon">
+    <br />
+    <input type="submit" value="Skicka">
+</form>
+</body>
+</html>
